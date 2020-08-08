eb0b7c5303d4f8cbcb4856ed3ae73a45a973716b
Jason Lenoble <jason@dev1.local.host>
2020-08-08 15:07:31 +0200

    write in .ts, transpile to .mjs

--- gulpfile-bootstrap.mjs
+++ gulpfile-bootstrap.mjs
@@ -3,5 +3,13 @@
 // Subsequently, simply run `gulp` as usual
 
 import gulp from "gulp";
+import babel from "gulp-babel";
+import rename from "gulp-rename";
 
-gulp.task("default", () => gulp.src("src/**/*.mjs").pipe(gulp.dest("gulp")));
+gulp.task("default", () =>
+  gulp
+    .src("src/**/*.ts")
+    .pipe(babel())
+    .pipe(rename({ extname: ".mjs" }))
+    .pipe(gulp.dest("gulp"))
+);
