c02ad08dc0b95234cf24cb510b3d97868a234ffb
Jason Lenoble <jason@dev1.local.host>
2020-08-09 21:54:25 +0200

    autoload tasks
    Any task defined in tasks subdir will be autoloaded

--- src/gulp.ts
+++ src/gulp.ts
@@ -1,18 +1,28 @@
-import gulp from "gulp";
-import debug from "gulp-debug";
+import path from "path";
+import globby from "globby";
 
-import "./tasks/clean";
-
-/**
- * Gulp
- */
 export class Gulp {
-  /**
-   * Runs the Gulp instance: To be called last.
-   *
-   * @returns {void}
-   */
+  readonly gulpDir: string;
+
+  constructor() {
+    this.gulpDir = path.join(process.cwd(), "gulp");
+
+    Object.freeze(this);
+  }
+
+  private _loadTasks() {
+    const taskFilePaths = globby.sync(this.path("./tasks/**/*.js"));
+
+    taskFilePaths.forEach((taskFilePath: string) => {
+      require(taskFilePath);
+    });
+  }
+
+  path(relPath: string): string {
+    return path.join(this.gulpDir, relPath);
+  }
+
   run(): void {
-    gulp.task("default", () => gulp.src("*").pipe(debug()));
+    this._loadTasks();
   }
 }
